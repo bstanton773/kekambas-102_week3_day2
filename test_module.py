d46e84210439010b958d3b133f87c6f3b9ee9dce
Brian <bstanton773@gmail.com>
2022-10-04 15:46:33 -0500

    Tuesday work

---
+++ test_module.py
@@ -0,0 +1,10 @@
+print('Hello this is the test module!')
+
+def greet(name):
+    print(f"Hello {name.title()}. How are you today? I hope you are doing well!")
+
+def leave(name):
+    print(f"Goodbye {name.title()}. It was a pleasure seeing you.")
+
+# greet('brian')
+# leave('brian')
